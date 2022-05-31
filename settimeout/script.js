b8a056162464a51c1dd2fc273ef45194452a34ec
SebaChuaqui <schuaquicano@gmail.com>
2022-05-31 17:58:13 -0300

    CallBack VII

---
+++ settimeout/script.js
@@ -0,0 +1,10 @@
+setTimeout(() => {
+    console.log('después de 2 segundos!')
+    }, 2000);
+    setTimeout(() => {
+    console.log('después de 3 segundos!')
+    }, 3000);
+    setTimeout(() => {
+    console.log('después de 1 segundos!')
+    }, 1000);
+    
